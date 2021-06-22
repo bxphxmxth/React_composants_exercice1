a42cbc06a80cecf86048153104b3545565c4029c
bxphxmxth <fushs@hotmail.fr>
2021-06-22 16:42:34 +0200

    sec1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import Header from "./components/Header"
+import Section1 from "./components/Section1"
 import './App.css';
 
 function App() {
   return (
-    
-    <Header/>
+
+    <div>
+      
+      <Header/>
+      <Section1/>
+
+      
+    </div>
   );
 }
 
---
+++ src/components/Section1.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Section1 extends React.Component{
+
+     render(){
+          return(
+               <section1 className="section1">
+                    
+                    <div className="tartine">
+                         <p className="logo">Tartine</p>
+
+                         <div>
+
+                         <h1>Mon premier article</h1>
+                         <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+
+                         </div>
+
+                    </div>
+
+                    <div className="tartine">
+                         <p className="logo">Tartine</p>
+
+                         <div>
+
+                         <h1>Mon premier article</h1>
+                         <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+
+                         </div>
+
+                    </div>
+
+               </section1>
+          )
+     }
+}
+
+
+
+export default Section1
