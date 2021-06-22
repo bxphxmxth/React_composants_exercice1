5b083b8ad9eee4b50db798d3f85011729fdabb07
bxphxmxth <fushs@hotmail.fr>
2021-06-22 16:49:49 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import Header from "./components/Header"
 import Section1 from "./components/Section1"
+import Footer from "./components/Footer"
 import './App.css';
 
 function App() {
   return (
 
     <div>
-      
+
       <Header/>
       <Section1/>
+      <Footer/>
 
       
     </div>
